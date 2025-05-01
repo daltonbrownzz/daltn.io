31d4222cd24707ac7c826f5b8a327821591a78b3
daltonbrownzz <bubbabrown1012@gmail.com>
2025-05-01 14:06:51 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Fade-in on scroll
+const sections = document.querySelectorAll('section');
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.animationPlayState = 'running';
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.2 });
+
+sections.forEach(section => {
+  observer.observe(section);
+});
